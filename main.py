e8e14b26553250d0a715ec3c0e6c0d0a1eee25fa
LILIANAPILLAJOUTM <149269276+LILIANAPILLAJOUTM@users.noreply.github.com>
2023-11-26 08:31:17 -0500

    impacto ecologico

---
+++ main.py
@@ -0,0 +1,20 @@
+numero1 = 7
+numero2 = 6
+multiplicacion = numero1 * numero2
+
+print(multiplicacion)
+
+
+#entrada de mensaje
+
+mensaje = input("digite el mensaje: ")
+print(f"muy bien(exelente)")
+
+
+
+
+print(5+4)
+print(9-4)
+print(13*4)
+print(25/5)
+
